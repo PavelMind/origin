be825fb5aa8a10542aa61b0be6163d496b237e84
Pavel Kovtun <planetovermind@mail.ru>
2024-11-29 19:56:21 +0300

    add lesson 7

---
+++ basic_programming/Lesson 07/task 1/task 1.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+#define MODE 1
+
+#ifndef MODE
+#error MODE не определена
+#endif
+
+#if MODE==1
+int add(int a, int b) { return a + b; }
+#endif 
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+#if MODE==0
+    std::cout << "Работаю в режиме тренировки";
+#elif MODE==1
+    int n1, n2;
+    std::cout << "Работаю в боевом режиме\n";
+    std::cout << "Введите число 1: ";
+    std::cin >> n1;
+    std::cout << "Введите число 2: ";
+    std::cin >> n2;
+    std::cout << "Результат сложения: " << add(n1, n2);
+#else
+    std::cout << "Неизвестный режим. Завершение работы";
+#endif
+
+
+}
