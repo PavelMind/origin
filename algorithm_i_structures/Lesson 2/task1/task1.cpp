400a47bbce19b9e419b706a71a29f2ebc4a858d3
Pavel Kovtun <planetovermind@mail.ru>
2025-01-15 12:45:41 +0300

    add blok Algoritm.. and add lesson 2

---
+++ algorithm_i_structures/Lesson 2/task1/task1.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+
+
+
+int fiboRec(int a, int b, int n, int max) {    
+    
+    if (n == max) {
+        return b;
+    }
+    else {
+        int sum = a + b;
+        return fiboRec(b, sum, ++n, max);
+    }
+    
+
+}
+
+int fibonachy(int max) {
+    if (max == 1) return 0;
+    if (max < 1) return -1;
+    return fiboRec(0, 1, 2, max);
+}
+
+// 0  1  1  2  3  5  8  13  21
+
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int m;
+    std::cout << "Введите номер числа: ";
+    std::cin >> m;
+    int res = fibonachy(m);
+    std::cout << "Число Фибоначчи: " << res << std::endl;
+    return 0;
+}
