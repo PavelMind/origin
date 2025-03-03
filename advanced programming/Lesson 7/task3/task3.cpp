9d65561c15200c0a17f58a3e479c72fa58a08a8b
Pavel Kovtun <planetovermind@mail.ru>
2025-03-03 19:14:41 +0300

    chang task3 in lesson 7

--- advanced programming/Lesson 7/task3/task3.cpp
+++ advanced programming/Lesson 7/task3/task3.cpp
@@ -1,44 +1,66 @@
 ﻿#include <iostream>
+#include <string>
 #include "vectory.hpp"
 
 
-
-
-
+template<typename T>
+void print(my::vector<T>& vec, std::string name) {
+    std::cout << name << "\nsize " << vec.size();
+    std::cout << "\ncapacity " << vec.capacity();
+    int sz = vec.size();
+    for (int i = 0; i < sz; ++i) {
+        std::cout << std::endl << " [" << i << "]: " << vec.at(i);
+    }
+    std::cout << "\n\n";
+}
 
 
 int main()
 {
     try {
         my::vector<int> v1;
-        std::cout << std::endl << "v1 size " << v1.size();
+        std::cout << "v1 size " << v1.size();
         std::cout << std::endl << "v1 capacity " << v1.capacity();
         //std::cout << std::endl << "v1 at[0] " << v1.at(0);// except
 
+        std::cout << std::endl << std::endl;
         my::vector<double> v2(5);
+        v2.push_back(2);
+        v2.push_back(4);
+        v2.push_back(6);
         v2.push_back(8);
-        std::cout << std::endl << "v2 size " << v2.size();
-        std::cout << std::endl << "v2 capacity " << v2.capacity();        
-        std::cout << std::endl << "v2 at[0] " << v2.at(0);
-#if 0
-        my::vector<int> v3{ 2, 4, 7 };
-        std::cout << std::endl << "v3 size " << v3.size();
-        std::cout << std::endl << "v3 capacity " << v3.capacity();
+        v2.push_back(10);                
+        std::cout << "v2 at[0] " << v2.at(0) <<"\n";
+        print<>(v2, "v2");
 
-        for (int i = 0; i < v3.size(); ++i) {
-            std::cout << std::endl << "v3 at[" << i << "]: " << v3.at(i);
-        }
-        v3.push_back(8);
+        my::vector<double> v3(4);
         v3.push_back(1);
-        v3.push_back(26);
+        v3.push_back(3);
+        v3.push_back(5);        
+        print<double>(v3, "v3");
 
-        for (int i = 0; i < v3.size(); ++i) {
-            std::cout << std::endl << "v3 at[" << i << "]: " << v3.at(i);
-        }
-        //std::cout << v3.at(10);//except
+        my::vector<double> v4(v3);          
+        print<double>(v4, "v4   (v3)");
 
+        v3 = v2;
+        std::cout << "v3=v2\n";        
+        print<double>(v3, "v3");
+
+        v2 = v4;
+        std::cout << "v2 = v4\n";
+        print<double>(v2, "v2");
+        
+#if 1
+        my::vector<int> v5{ 2, 4, 7 };
+        print<>(v5, "v5");
+        v5.push_back(8);
+        v5.push_back(1);
+        v5.push_back(26);
+
+        print<>(v5, "v5");
+        
+        //std::cout << "v6";  my::vector<short> v6(0);//exept
 #endif
-
 
     }
     catch (std::exception e) {
