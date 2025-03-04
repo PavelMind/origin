8cdb9f7defde02c2409c0fa16d00c9e74a03e79a
Pavel Kovtun <planetovermind@mail.ru>
2025-03-04 21:00:57 +0300

    add lesson 8

---
+++ advanced programming/Lesson 8/task2/task2.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <set>
+#include <list>
+#include <vector>
+
+template<typename Cont>
+void print_container(Cont& cont) {
+    for (auto i : cont) {
+        std::cout << i <<" ";
+    }
+    std::cout << "\n\n";
+}
+
+
+int main()
+{
+    std::set<std::string> test_set = { "one", "two", "three", "four" };
+    print_container(test_set);
+
+    std::list<std::string> test_list = { "one", "two", "three", "four" };
+    print_container(test_list);
+
+    std::vector<std::string> test_vector = { "one", "two", "three", "four" };
+    print_container(test_vector);
+}
