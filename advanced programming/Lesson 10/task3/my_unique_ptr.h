297c7697f0b103cde5236ed35d15c89d1adf7a36
Pavel Kovtun <planetovermind@mail.ru>
2025-03-07 17:22:00 +0300

    add lesson 10

---
+++ advanced programming/Lesson 10/task3/my_unique_ptr.h
@@ -0,0 +1,31 @@
+#pragma once
+
+namespace my {
+
+    template<typename T>
+    class unique_ptr {
+        T* ptr;
+    public:
+        unique_ptr(T* p): ptr(p){}
+        ~unique_ptr() {
+            if(ptr) delete ptr;
+        }
+        unique_ptr(unique_ptr&) = delete;
+        void operator =(unique_ptr&) = delete;
+
+        unique_ptr(unique_ptr&& other) {
+            ptr = other.ptr;
+            other.ptr = nullptr;
+        }
+
+        T* operator *() {
+            return ptr;
+        }
+        T* release() {
+            T* temp = ptr;
+            ptr = nullptr;
+            return temp;
+        }
+    };
+
+}
---
+++ advanced programming/Lesson 10/task3/task3.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include "my_unique_ptr.h"
+#include <utility>
+
+class exampl {    
+public:
+    char a;
+    exampl(char A) : a(A) { std::cout << "Constructor- " << a << std::endl; }
+    ~exampl()  { std::cout << " destructor~ " << a << std::endl; }
+};
+
+
+int main()
+{
+    exampl r('r');
+
+    my::unique_ptr<exampl> g = new exampl('g');
+    my::unique_ptr<exampl> h = new exampl('h');
+    //g = h;
+    //my::unique_ptr<exampl> e(g);
+    //my::unique_ptr<exampl> p = h;
+
+    {
+        my::unique_ptr<exampl> v = new exampl('v');
+        (*v)->a = 'w';
+    }
+    my::unique_ptr<exampl> y = new exampl('y');
+    my::unique_ptr<exampl> mov(std::move(y));
+
+    exampl* smpl = h.release();
+    delete smpl;
+}
