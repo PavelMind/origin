02fd30b7df1e6fddd2b433c399f1bf78d3170102
Pavel Kovtun <planetovermind@mail.ru>
2025-06-25 20:02:56 +0300

    add lesson 4

---
+++ design patterns/Lesson 4/task 2/newVariant.h
@@ -0,0 +1,74 @@
+#pragma once
+#include <fstream>
+
+
+class PrintTypeI {
+public:
+    virtual ~PrintTypeI() = default;
+    virtual std::string print(std::string&) const = 0;
+};
+
+class printAsHTML :public PrintTypeI {
+public:
+    std::string print(std::string& str) const override
+    {
+        return "<html>" + str + "<html/>";
+    }
+};
+
+class printAsText :public PrintTypeI {
+public:
+    std::string print(std::string& str) const override
+    {
+        return str;
+    }
+};
+
+class printAsJSON :public PrintTypeI {
+public:
+    std::string print(std::string& str) const override
+    {
+        return "{ \"data\": \"" + str + "\"}";
+    }
+};
+
+class dataPrintableInt
+{
+public:
+    virtual ~dataPrintableInt() = default;
+    virtual std::string print(PrintTypeI*) = 0;
+};
+
+class Data : public dataPrintableInt
+{
+public:
+    Data(std::string data) : data_(std::move(data)) {}
+    std::string print(PrintTypeI* typePrint) override {
+        return typePrint->print(data_);
+    }
+    
+private:
+    std::string data_;
+};
+
+
+
+void saveTo(std::ofstream& file, dataPrintableInt* printable, PrintTypeI* pType)
+{
+    file << printable->print(pType)<<"\n";
+}
+
+void saveToAsHTML(std::ofstream& file,  dataPrintableInt* printable) {
+    PrintTypeI* pTyp = new printAsHTML;
+    saveTo(file, printable, pTyp);
+}
+
+void saveToAsJSON(std::ofstream& file, dataPrintableInt* printable) {
+    PrintTypeI* pTyp = new printAsJSON;
+    saveTo(file, printable, pTyp);
+}
+
+void saveToAsText(std::ofstream& file, dataPrintableInt* printable) {
+    PrintTypeI* pTyp = new printAsText;
+    saveTo(file, printable, pTyp);
+}
---
+++ design patterns/Lesson 4/task 2/task 2.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include <fstream>
+#include "newVariant.h"
+
+
+int main()
+{
+    std::ofstream file("filik.txt", std::ios::app);
+    Data textForOut("Hello, good code");
+    saveToAsHTML(file, &textForOut);
+    saveToAsJSON(file, &textForOut);
+    saveToAsText(file, &textForOut);
+}
