1cc44d5bb4da0a5b4fb32213dde605aa06d878b5
Pavel Kovtun <planetovermind@mail.ru>
2025-12-24 10:48:17 +0300

    correct ignored

--- diploma thesis - Crawler/cpp-httplib-0.24.0-copy/test/include_httplib.cc
+++
@@ -1,5 +0,0 @@
-// The sole purpose of this file is to include httplib_S.h in a separate
-// compilation unit, thus verifying that inline keywords have not been forgotten
-// when linked together with test.cc.
-
-#include <httplib_S.h>
--- diploma thesis - Crawler/cpp-httplib-0.24.0-copy/test/include_windows_h.cc
+++
@@ -1,6 +0,0 @@
-// Test if including windows.h conflicts with httplib_S.h
-
-#define WIN32_LEAN_AND_MEAN
-#include <windows.h>
-
-#include <httplib_S.h>
